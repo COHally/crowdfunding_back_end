4c9a65f786e945d1d166bb2d258c922bf4fa2cfb
COHALLY <CATHY.O'HALLORAN@RWWA.COM.AU>
2024-01-22 11:42:39 +0800

    -Deployment.

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -61,8 +61,8 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'users.CustomUser'
 
 REST_FRAMEWORK = {
-'DEFAULT_AUTHENTICATION_CLASSES': [
-'rest_framework.authentication.TokenAuthentication',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
 ]
 }
 
