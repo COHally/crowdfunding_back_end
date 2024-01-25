c7df6163f6d273576cc71af585c286b66a99453e
COHALLY <CATHY.O'HALLORAN@RWWA.COM.AU>
2024-01-25 15:32:17 +0800

    Update user function.

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -28,4 +28,22 @@ class CustomUserDetail(APIView):
         user = self.get_object(pk)
         serializer = CustomUserSerializer(user)
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+        user = self.get_object(pk)
+        serializer = CustomUserSerializer(
+            instance=user,
+            data=request.data,
+            partial=True
+    )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+    )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
