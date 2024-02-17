926c08e4dbc83183c262d142a4aff9eb3756ec37
COHALLY <CATHY.O'HALLORAN@RWWA.COM.AU>
2024-02-17 15:36:34 +0800

    Project create update.

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('projects/', views.ProjectList.as_view()),
     path('projects/<int:pk>/', views.ProjectDetail.as_view()),
-    path('projects/<int:pk>/create-project/', views.CreateProject.as_view()),
+    path('project/create-project/', views.CreateProject.as_view()),
     path('pledges/', views.PledgeList.as_view()),
     path('pledges/<int:pk>/', views.PledgeDetail.as_view())
 ]
