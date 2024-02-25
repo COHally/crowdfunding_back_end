6684726e32fdecb1af3bfad2d7bb2a6747ddd745
COHALLY <CATHY.O'HALLORAN@RWWA.COM.AU>
2024-02-26 07:43:59 +0800

    Auth amendments.

--- crowdfunding/crowdfunding/views.py
+++ crowdfunding/crowdfunding/views.py
@@ -1,29 +1,31 @@
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
 class CustomAuthToken(ObtainAuthToken):
 
     def post(self, request, *args, **kwargs):
-                # Call super to get the token
+        # Call super to get the token
         response = super().post(request, *args, **kwargs)
 
-        # Extract token from the response
-        token = response.data['token']
+        if 'token' in response.data:
+            # Extract token from the response
+            token = response.data['token']
 
-        # Extract user details
-        user_id = request.user.id
-        user_email = request.user.email
-        user_first_name = request.user.first_name
-        user_last_name = request.user.last_name
+            # Extract user details
+            user_id = request.user.id if request.user else None
+            user_email = request.user.email if request.user else None
+            user_first_name = request.user.first_name if request.user else None
+            user_last_name = request.user.last_name if request.user else None
 
-        # add more user details to response as needed
-        custom_response = {
-            'token': token,
-            'user_id': user_id,
-            'email': user_email,
-            'first_name': user_first_name,
-            'last_name': user_last_name
-        }
+            # add more user details to response as needed
+            custom_response = {
+                'token': token,
+                'user_id': user_id,
+                'email': user_email,
+                'first_name': user_first_name,
+                'last_name': user_last_name
+            }
 
-        return Response(custom_response)
+            return Response(custom_response)
+
+        return response
