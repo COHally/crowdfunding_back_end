27078a7b5879f484a710cf605f8835d19f283b9c
COHALLY <CATHY.O'HALLORAN@RWWA.COM.AU>
2024-02-26 07:16:51 +0800

    Auth amendment.

---
+++ crowdfunding/crowdfunding/views.py
@@ -0,0 +1,29 @@
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+                # Call super to get the token
+        response = super().post(request, *args, **kwargs)
+
+        # Extract token from the response
+        token = response.data['token']
+
+        # Extract user details
+        user_id = request.user.id
+        user_email = request.user.email
+        user_first_name = request.user.first_name
+        user_last_name = request.user.last_name
+
+        # add more user details to response as needed
+        custom_response = {
+            'token': token,
+            'user_id': user_id,
+            'email': user_email,
+            'first_name': user_first_name,
+            'last_name': user_last_name
+        }
+
+        return Response(custom_response)
