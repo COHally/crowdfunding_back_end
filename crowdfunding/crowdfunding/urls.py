6eb1a5ba068173fc47cdf028a562f9508a943908
COHALLY <CATHY.O'HALLORAN@RWWA.COM.AU>
2024-02-26 11:36:07 +0800

    Auth amendments.

--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-
+from users.views import CustomAuthToken
 
 
 urlpatterns = [
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', include('projects.urls')),
     path('', include('users.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', CustomAuthToken.as_view(), name='api_token_auth'),
 ]
--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 
 
 class CustomUserList(APIView):
@@ -57,5 +59,15 @@ class CustomUserDetail(APIView):
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
+class CustomAuthToken(ObtainAuthToken):
 
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk
+        })
 
