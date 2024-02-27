3df985ffe9b510ffedbe64db43f2c5a6eb48b97b
COHALLY <CATHY.O'HALLORAN@RWWA.COM.AU>
2024-02-27 19:05:43 +0800

    Auth amendment.

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -6,8 +6,6 @@ from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSeria
 from django.http import Http404
 from rest_framework import status, permissions
 from .permissions import IsOwnerOrReadOnly
-from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.views import ObtainAuthToken
 
 
 class ProjectList(APIView):
@@ -138,17 +136,6 @@ class PledgeDetail(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
     
-class CustomAuthToken(ObtainAuthToken):
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key,
-            'user_id': user.pk,
-            'username': user.username
-        })
     
 
